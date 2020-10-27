0f94f7c16297e520f30047c08c1dcc22c5bbe856
Arsalan Salari <ars.salari@gmail.com>
2020-10-27 21:08:58 +0330

    fix bugs and stuff

--- openvqa/premodels/preprocess_dataset.py
+++ openvqa/premodels/preprocess_dataset.py
@@ -4,12 +4,24 @@ import os
 import numpy as np
 from PIL import Image
 from .resnet.preproc import preproc_to_feats
+import time
 
 
 def preprocess_dataset(data_path = '/content/vqa',dest_path='/content/openvqa/data/vqa', preproc=preproc_to_feats):
-	for p in Path(data_path).rglob('*.jpg'):
-		image = Image.open(p)
-		res = preproc(image)
-		destination = os.path.join(dest_path, p[len(data_path), :]).replace('jpg', 'npz')
-		np.savez(dest, res)
-		os.unlink(p)
+  start_time = time.time()
+  c=0
+  for p in Path(data_path).rglob('*.jpg'):
+    c +=1
+    if c % 100 == 0: 
+      print('processed {} files'.format(c))
+    image = Image.open(p).convert('RGB')
+    res = preproc(image)
+    destination = os.path.join(dest_path, p.relative_to(*Path(data_path).parts)).replace('jpg', 'npz')
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
+    np.savez(destination, res)
+    os.unlink(p)
+  print('time passed: ', time.strftime('%H:%M:%S', time.gmtime(time.time() - time_start)))
+
+
+if __name__ == '__main__': 
+  preprocess_dataset()
